c6fab53da5700f9df4b6d01a03e604ddee942c17
jacobhamilton12 <hamilton1jake@gmail.com>
2020-07-06 12:43:13 -0500

    can insert events in database not read

---
+++ events.php
@@ -0,0 +1,28 @@
+<?php
+require 'connect.php';
+
+$postdata = file_get_contents("php://input");
+$uemail = "";
+
+if(isset($postdata) && !empty($postdata) && isset($_SESSION['userId']) && isset($_SESSION['userEmail'])){
+    $uemail = $_SESSION['userEmail'];
+    $request = json_decode($postdata);
+    $name = $request->name;
+    $description = $request->desc;
+    $date = $request->date;
+    echo "success";
+}else{
+    echo "Not logged in";
+    exit();
+}
+
+$sql = "INSERT INTO `events` (`name`, `user`, `description`, `date`) VALUES (?, ?, ?, ?)";
+$stmt = mysqli_stmt_init($conn);
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_bind_param($stmt, "ssss", $name, $uemail, $description, $date);
+mysqli_stmt_execute($stmt);
+
+http_response_code(201);
+
+mysqli_close($conn);
+exit;
